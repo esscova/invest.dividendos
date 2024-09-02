a6c3a1f8210642ae749d82273aa43b027ba7ea28
wellington moreira <wsantos08@hotmail.com>
2024-09-02 18:54:27 -0300

    adicionado: dependencia para transactions

--- app/core/deps.py
+++ app/core/deps.py
@@ -1,9 +1,13 @@
+from fastapi import Depends
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from core.settings import settings
-from fastapi import Depends
-from services.user_services import UserService
 
+from core.settings import settings
+from services.user_services import UserService
+from services.transactions_services import TransactionService
+
+#...
 engine = create_engine(settings.DATABASE_URL)
 
 def get_session():
@@ -12,3 +16,6 @@ def get_session():
 
 def user_service(session:Session = Depends(get_session)) -> UserService:
     return UserService(session)
+
+def get_transaction_service(session:Session = Depends(get_session)) -> TransactionService:
+    return TransactionService(session)
